231d1b78f425edf0abcfdc25bfa44af87bc3c160
lpeihan <291630202@qq.com>
2019-04-22 23:33:54 +0800

    Update 7. Reverse Integer.js

--- 7. Reverse Integer.js
+++ 7. Reverse Integer.js
@@ -2,19 +2,33 @@
  * @param {number} x
  * @return {number}
  */
+// var reverse = function(x) {
+//     let sum = 0;
+//     const MAX_VALUE = Math.pow(2, 31) - 1;
+//     const MIN_VALUE = Math.pow(-2, 31);
+    
+//     while (x) {
+//         sum = sum * 10 + x % 10;
+//         x = x / 10 | 0;
+        
+//         if (sum < MIN_VALUE || sum > MAX_VALUE) {
+//             return 0;
+//         } 
+//     }
+    
+//     return sum;
+// };
+
 var reverse = function(x) {
-    let sum = 0;
     const MAX_VALUE = Math.pow(2, 31) - 1;
     const MIN_VALUE = Math.pow(-2, 31);
+    const flag = x > 0;
     
-    while (x) {
-        sum = sum * 10 + x % 10;
-        x = x / 10 | 0;
-        
-        if (sum < MIN_VALUE || sum > MAX_VALUE) {
-            return 0;
-        } 
-    }
+    x = Math.abs(x);
     
-    return sum;
-};
+    x = parseInt(x.toString().split('').reverse().join(''));
+    
+    const res = flag ? x : -x;
+    
+    return res > MIN_VALUE && res < MAX_VALUE ? res : 0;
+}
