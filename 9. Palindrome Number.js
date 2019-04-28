dfe4b7097a2feb825fe4e136c999118a328026dc
李培汉 <lipeihan@chandashi.com>
2019-04-28 15:59:44 +0800

    Create 9.Palindrome Number.js

---
+++ 9. Palindrome Number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {    
+  const str = String(x);
+  let left = 0;
+  let right = str.length - 1;
+  
+  while (left <= right) {
+      if (str[left++] != str[right--]) {
+          return false;
+      }
+  }
+  
+  return true;
+};
