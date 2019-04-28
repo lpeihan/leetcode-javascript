6ab45f706dd23768cf50b767afa23a9da64ca654
李培汉 <lipeihan@chandashi.com>
2019-04-28 18:17:30 +0800

    Create 020. Valid Parentheses.js

---
+++ src/020. Valid Parentheses.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  const left = new Map([['(', 1], ['{', 2], ['[', 3]]);
+  const right = new Map([[')', 1], ['}', 2], [']', 3]]);
+
+  const arr = [];
+
+  for (let i = 0; i < s.length; i++) {
+    const val = s[i];
+
+    if (left.get(val)) {
+      arr.push(left.get(val));
+    } else {
+      if (arr.pop() !== right.get(val)) {
+        return false;
+      }
+    }
+  }
+
+  return arr.length === 0;
+};
