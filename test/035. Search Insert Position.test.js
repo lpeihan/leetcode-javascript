0c00ff6c1c53b456d8cdfcff8328174411255396
李培汉 <lipeihan@chandashi.com>
2019-04-29 14:29:29 +0800

    feat: 035

---
+++ test/035. Search Insert Position.test.js
@@ -0,0 +1,8 @@
+import { searchInsert } from '../src/035. Search Insert Position';
+
+test('searchInsert', () => {
+  expect(searchInsert([1, 3, 5, 6], 5)).toBe(2);
+  expect(searchInsert([1, 3, 5, 6], 2)).toBe(1);
+  expect(searchInsert([1, 3, 5, 6], 7)).toBe(4);
+  expect(searchInsert([1, 3, 5, 6], 0)).toBe(0);
+});
