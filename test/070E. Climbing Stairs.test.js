7a3a7c880d4ce6ad544fb4e1fc14c6ef115caba0
李培汉 <lipeihan@chandashi.com>
2019-05-02 23:50:17 +0800

    perf: 070E add jest

--- test/070E. Climbing Stairs.test.js
+++ test/070E. Climbing Stairs.test.js
@@ -1,4 +1,8 @@
-import { climbStairs, climbStairs_0 } from '../src/070E. Climbing Stairs';
+import {
+  climbStairs,
+  climbStairs_0,
+  climbStairs_1
+} from '../src/070E. Climbing Stairs';
 
 test('climbStairs', () => {
   expect(climbStairs(2)).toBe(2);
@@ -8,4 +12,9 @@ test('climbStairs', () => {
 test('climbStairs_0', () => {
   expect(climbStairs_0(2)).toBe(2);
   expect(climbStairs_0(44)).toBe(1134903170);
+});
+
+test('climbStairs_1', () => {
+  expect(climbStairs_1(2)).toBe(2);
+  expect(climbStairs_1(44)).toBe(1134903170);
 });
