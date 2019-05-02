515bd2a22f216820d828b62166d63cb8c389dcdd
李培汉 <lipeihan@chandashi.com>
2019-05-02 23:43:23 +0800

    feat: 070E

--- test/070E. Climbing Stairs.test.js
+++ test/070E. Climbing Stairs.test.js
@@ -1,6 +1,11 @@
-import { climbStairs } from '../src/070E. Climbing Stairs';
+import { climbStairs, climbStairs_0 } from '../src/070E. Climbing Stairs';
 
 test('climbStairs', () => {
   expect(climbStairs(2)).toBe(2);
   expect(climbStairs(44)).toBe(1134903170);
+});
+
+test('climbStairs_0', () => {
+  expect(climbStairs_0(2)).toBe(2);
+  expect(climbStairs_0(44)).toBe(1134903170);
 });
