637d93529eb7a2f30e1fcf379b799b07dc4b58e6
李培汉 <lipeihan@chandashi.com>
2019-05-03 22:16:41 +0800

    perf: 完善测试用例

--- test/001E. Two Sum.test.js
+++ test/001E. Two Sum.test.js
@@ -2,8 +2,10 @@ import { twoSum, twoSum_0 } from '../src/001E. Two Sum';
 
 test('twoSum_0', () => {
   expect(twoSum_0([2, 7, 11, 15], 9)).toEqual([0, 1]);
+  expect(twoSum_0([2, 7, 11, 15], 10)).toBe(null);
 });
 
 test('twoSum', () => {
   expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
+  expect(twoSum([2, 7, 11, 15], 10)).toBe(null);
 });
