b437933bcc6207edc29680377f6a71ad3db968fc
lipeihan <291630202@qq.com>
2019-05-07 21:31:53 +0800

    feat: 326E

---
+++ src/326E. Power of Three.js
@@ -0,0 +1,16 @@
+/**
+ * https://leetcode.com/problems/power-of-three/
+ * @param {number} n
+ * @return {boolean}
+ */
+export var isPowerOfThree = function(n) {
+  if (n === 0) {
+    return false;
+  }
+
+  while (n % 3 === 0) {
+    n /= 3;
+  }
+
+  return n === 1;
+};
---
+++ test/326E. Power of Three.test.js
@@ -0,0 +1,6 @@
+import { isPowerOfThree } from '../src/326E. Power of Three';
+
+test('isPowerOfThree', () => {
+  expect(isPowerOfThree(27)).toBe(true);
+  expect(isPowerOfThree(45)).toBe(false);
+});
