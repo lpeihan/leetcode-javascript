ae98e11d35e9c5f2842e33fdf213a1dab252dcd1
李培汉 <lipeihan@chandashi.com>
2020-04-21 14:14:26 +0800

    回溯算法优化

--- notes/backtrack.js
+++ notes/backtrack.js
@@ -2,19 +2,20 @@ const candidates = [2, 5, 10, 2, 4, 3];
 const target = 7;
 
 var combinationSum2 = function(candidates, target) {
-  candidates.sort((a, b) => a - b);
-  var result = [];
-  var n = candidates.length;
+  const result = [];
 
   function backtrack(start, sum, list) {
     if (sum === target) {
       result.push(list);
+      return;
     }
-    for (let i = start; i < n; i++) {
-      if (candidates[i] + sum > target) break;
-      // 相同数字只允许循环的第一个递归，避免重复
-      // if (candidates[i] === candidates[i - 1] && i > start) continue;
-      backtrack(i + 1, sum + candidates[i], [...list, candidates[i]]);
+
+    for (let i = start; i < candidates.length; i++) {
+      const val = candidates[i];
+
+      if (sum + val <= target) {
+        backtrack(i + 1, sum + val, [...list, val]);
+      }
     }
   }
 
